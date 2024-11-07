aa727ca2b0ca62616065edcd0eda47b3e0ad028a
itchydonut08 <147563728+itchydonut08@users.noreply.github.com>
2024-11-07 18:38:14 -0500

    Add files via upload

---
+++ tlist/tlist.py
@@ -0,0 +1,18 @@
+print 'Tlist:A Tool List by itchydonut08'
+print '    ___     ___  ___ ___           '
+print '     |  |    |   |__  |            '
+print '     |  |    |     |  |            '
+print '     |  |__ _|_  __|  |            '
+print '[1]Social enginering'
+print '[2]Exploitation'
+print '[3]Sniffing and spoofing'
+print '[4]Wifi exploitation'
+input = input('Choose:')
+if input==1 :
+	print 'setoolkit'
+if input==2 :
+	print 'metasploit'
+if input==3 :
+	print 'nmap'
+if input==4 :
+	print 'wifi pineapple'
